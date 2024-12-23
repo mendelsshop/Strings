4e5614988044331676e8e5515bd87f75bc339d07
mendelsshop <mendelsshop@gmail.com>
2024-12-23 18:47:15 -0500

    more work on identifiers and comments

--- lib/parser2.ml
+++ lib/parser2.ml
@@ -7,12 +7,17 @@ end
 module Parser = AMPCL.Parser.Char.String.Show.Make (Unit)
 open Parser
 
+(* function composition *)
+let ( & ) f g x = f (g x)
+
 (* function wise or - takes two functions that return boolean and ors when given an input *)
 let ( |-> ) f g x = f x || g x
-let ( & ) f g x = f (g x)
 
 (* function wise and - takes two functions that return boolean and ands when given an input *)
 let ( &-> ) f g x = f x && g x
+
+(* function wise not - takes a function that returns a boolean and nots it when given an input *)
+let ( !-> ) f = not & f
 let is_lower_case_letter = ( <= ) 'a' &-> ( >= ) 'z'
 let is_upper_case_letter = ( <= ) 'A' &-> ( >= ) 'Z'
 let is_letter = is_upper_case_letter |-> is_lower_case_letter
@@ -24,6 +29,20 @@ let string_of_char = String.make 1
 let basic_identifier =
   letter <$> string_of_char >>= fun first ->
   takeWhile is_hexadecimal <$> ( ^ ) first <?> "identifier"
+
+let infix_identifier =
+  let not_infix_symbols =
+    is_hexadecimal
+    |-> (Fun.flip List.mem) [ '('; ')'; '['; ']'; '\"'; '{'; '}' ]
+  in
+  let infix_symbols = !->not_infix_symbols in
+  sat infix_symbols <$> string_of_char >>= fun start ->
+  takeWhile infix_symbols <$> ( ^ ) start
+
+let identifier =
+  basic_identifier
+  <|> (* TODO: junk/comments *)
+  between (char '(') (char ')') infix_identifier
 
 let number_inner = takeWhile is_decimal
 let number1_inner = check (( > ) 0 & String.length) (takeWhile is_decimal)
@@ -38,3 +57,11 @@ let float =
         char '.' << number1_inner <$> fun rest ->
         first ^ "." ^ rest |> float_of_string )
   <?> "float"
+
+let line_comment =
+  between (string "--")
+    (char '\n' |> opt)
+    (takeWhile (( <> ) '\n' |-> ( <> ) '\"'))
+
+let multi_line_comment = between (char '#') (char '#') (takeWhile (( <> ) '#'))
+let comment = line_comment <|> multi_line_comment
