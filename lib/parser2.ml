f0b8fce2659a28779b894747e086baf56aa53a74
mendelsshop <mendelsshop@gmail.com>
2024-12-25 21:44:26 -0500

    new type parser mostly done

--- lib/parser2.ml
+++ lib/parser2.ml
@@ -49,11 +49,13 @@ let white_space = takeWhile is_white_space
 let comment = line_comment <|> multi_line_comment
 let junk = many (comment <|> white_space)
 
-let basic_identifier =
-  junk << letter <$> string_of_char >>= fun first ->
+let basic_identifier_without_junk =
+  letter <$> string_of_char >>= fun first ->
   takeWhile is_hexadecimal <$> ( ^ ) first
   |> check (not_in keywords)
   <?> "identifier"
+
+let basic_identifier = junk << basic_identifier_without_junk
 
 let infix_identifier =
   let not_infix_symbols =
@@ -61,7 +63,7 @@ let infix_identifier =
     |-> (Fun.flip List.mem) [ '('; ')'; '['; ']'; '\"'; '{'; '}' ]
   in
   let infix_symbols = !->not_infix_symbols in
-  junk << sat infix_symbols <$> string_of_char >>= fun start ->
+  junk << sat (infix_symbols &-> ( <> ) '`') <$> string_of_char >>= fun start ->
   takeWhile infix_symbols <$> ( ^ ) start
   |> check (not_in reserved_operators)
   <?> "infix identifier"
@@ -71,6 +73,7 @@ let identifier =
   <|> (* TODO: junk/comments *)
   between (junk << char '(') (junk << char ')') infix_identifier
 
+let variant_identifier = junk << char '`' << identifier
 let number_inner = takeWhile is_decimal
 let number1_inner = check (( > ) 0 & String.length) (takeWhile is_decimal)
 let number = junk << number1_inner <$> int_of_string <?> "number"
@@ -99,13 +102,16 @@ let record p identifier_short_hand assign =
   between (junk << char '{') (junk << char '}') (sepby field (junk << char ';'))
 
 (* variant parser is only used for types so it won't be ambiguous with application parser *)
-let variant p =
-  opt (junk << char '|') << seq identifier p >>= fun first ->
-  many (junk << char '|' << seq identifier p) <$> List.cons first
-
-let constructor = seq identifier
+let constructor = seq variant_identifier
 
 let rec typeP =
+  let list_to_row =
+    Fun.flip
+      (List.fold_right (fun (name, ty) row ->
+           TRowExtension { label = name; field = ty; row_extension = row }))
+      TEmptyRow
+  in
+
   Parser
     {
       unParse =
@@ -116,19 +122,34 @@ let rec typeP =
                 unit <$> Fun.const TUnit;
                 paren typeP;
                 junk << string "int" <$> Fun.const TInteger;
-                junk << string "float" <$> Fun.const TInteger;
-                junk << string "string" <$> Fun.const TInteger;
-                junk << string "bool" <$> Fun.const TInteger;
-                ( record typeP None ':'
-                <$> Fun.flip
-                      (List.fold_right (fun (name, ty) row ->
-                           TRowExtension
-                             { label = name; field = ty; row_extension = row }))
-                      TEmptyRow
-                <$> fun row -> TRecord row );
+                junk << string "float" <$> Fun.const TFloat;
+                junk << string "string" <$> Fun.const TString;
+                junk << string "bool" <$> Fun.const TBool;
+                record typeP None ':'
+                <$> ((fun row -> TRecord row) & list_to_row)
+                <?> "record";
               ]
           in
-          let (Parser { unParse }) = basic_type in
+          let tuple =
+            sepby1 basic_type (junk << char '*') <$> function
+            | t :: [] -> t
+            | t -> TTuple t
+          in
+          let variant =
+            many1
+              (seq variant_identifier
+                 (opt basic_type <$> Option.value ~default:TUnit))
+            <$> ((fun row -> TVariant row) & list_to_row)
+          in
+          let functionP =
+            variant <|> tuple >>= fun first ->
+            opt (junk << string "->" << typeP)
+            <$> Option.fold
+                  ~some:(fun return -> TFunction (first, return))
+                  ~none:first
+          in
+
+          let (Parser { unParse }) = functionP in
           unParse s ok err);
     }
 
