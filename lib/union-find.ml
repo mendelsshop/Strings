708f65cd818c0545a9edef34d4b1d28f0a75a14a
mendelsshop <mendelsshop@gmail.com>
2025-08-10 01:11:17 -0400

    more union find

--- lib/union-find.ml
+++
@@ -1,9 +0,0 @@
-type 'a root = { mutable rank : int; mutable data : 'a }
-
-type 'a parent_pointer_tree = [ `node of 'a elem | `root of 'a root ]
-and 'a elem = { mutable parent : 'a parent_pointer_tree ref }
-
-let make data = ref (`root { rank = 0; data })
-
-let rec find_set (x : 'a parent_pointer_tree) =
-  match x with `root _ as x -> x | `leaf { parent } -> find_set !parent
---
+++ lib/union_find.ml
@@ -0,0 +1,28 @@
+type 'a root = { mutable rank : int; mutable data : 'a }
+
+type 'a parent_pointer_tree = [ `node of 'a node | `root of 'a root ]
+and 'a node = { mutable parent : 'a parent_pointer_tree ref }
+
+type 'a elem = 'a parent_pointer_tree ref
+
+let make data = ref (`root { rank = 0; data })
+
+(* we return the root node as a reference and as dereferenced to use fancy polymorphic variants matching as root is always a root *)
+let rec find_set (x : 'a elem) : 'a elem * [< `root of 'a root ] =
+  match !x with
+  | `root _ as x' -> (x, x')
+  | `node link ->
+      let res_ref, res = find_set link.parent in
+      link.parent <- res_ref;
+      (res_ref, res)
+
+(* let link x y =  *)
+
+let union x y =
+  let x_ref, `root x = find_set x in
+  let y_ref, `root y = find_set y in
+  if x_ref == y_ref then x_ref
+  else if x.rank > y.rank then (
+    y_ref := `node { parent = x_ref };
+    x_ref)
+  else failwith ""
